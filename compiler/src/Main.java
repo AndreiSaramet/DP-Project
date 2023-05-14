8772a7c810ce8bef3a791ab61d18bfbe1e078f55
AndreiSaramet <andreisaramet21@gmail.com>
2023-05-15 01:54:24 +0300

    main update

--- compiler/src/Main.java
+++ compiler/src/Main.java
@@ -1,5 +1,8 @@
+import controller.Controller;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Controller().run();
     }
 }
