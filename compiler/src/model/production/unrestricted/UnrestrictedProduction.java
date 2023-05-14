c44ca7c91ad6456e33bda664952c33e042d8825e
AndreiSaramet <andreisaramet21@gmail.com>
2023-05-14 21:34:15 +0300

    model production refactor unrestricted production

--- compiler/src/model/production/unrestricted/UnrestrictedProduction.java
+++ compiler/src/model/production/unrestricted/UnrestrictedProduction.java
@@ -1,29 +1,31 @@
 package model.production.unrestricted;
 
-import model.production.Production;
+import model.production.AbstractProduction;
 import model.production.builder.ProductionBuilder;
 import model.production.builder.ProductionBuilderImpl;
 import model.vocabulary.Symbol;
 
 import java.util.List;
 
-public interface UnrestrictedProduction extends Production {
-    static boolean isInstance(final Production production) {
-        return production instanceof UnrestrictedProduction;
+public class UnrestrictedProduction extends AbstractProduction {
+    protected UnrestrictedProduction(final List<? extends Symbol> leftSide, final List<? extends Symbol> rightSide) {
+        super(leftSide, rightSide);
     }
 
-    static void validateLeftSide(final List<? extends Symbol> leftSide) {
-        Production.validateLeftSide(leftSide);
+    @Override
+    protected void validateLeftSide(List<? extends Symbol> leftSide) {
+        super.validateLeftSide(leftSide);
         if (leftSide.isEmpty()) {
             throw new IllegalArgumentException(String.format("The left side of %s must not be empty", UnrestrictedProduction.class.getName()));
         }
     }
 
-    static void validateRightSide(final List<? extends Symbol> rightSide) {
-        Production.validateRightSide(rightSide);
+    @Override
+    protected void validateRightSide(List<? extends Symbol> rightSide) {
+        super.validateRightSide(rightSide);
     }
 
-    static ProductionBuilder<? extends UnrestrictedProduction> builder() {
-        return new ProductionBuilderImpl<>(UnrestrictedProductionImpl::new);
+    public static ProductionBuilder<? extends UnrestrictedProduction> builder() {
+        return new ProductionBuilderImpl<>(UnrestrictedProduction::new);
     }
 }
