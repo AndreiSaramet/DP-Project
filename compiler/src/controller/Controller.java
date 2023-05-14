edb80c8ea100505c61c19b7a9e1bf2b73b630295
AndreiSaramet <andreisaramet21@gmail.com>
2023-05-15 02:30:41 +0300

    controller update

--- compiler/src/controller/Controller.java
+++ compiler/src/controller/Controller.java
@@ -2,7 +2,7 @@ package controller;
 
 import command.Command;
 import command.CommandImpl;
-import model.grammar.Grammar;
+import model.grammar.ContextFreeGrammar;
 import model.grammar.reader.ContextFreeGrammarReader;
 import model.production.context_free.ContextFreeProduction;
 import view.View;
@@ -10,24 +10,17 @@ import view.View;
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Controller {
-    public Controller() {
-    }
-
     private final View view = new View();
 
-    private Grammar<? extends ContextFreeProduction> model = null;
+    private ContextFreeGrammar<? extends ContextFreeProduction> model = null;
 
     private final Map<String, Command> commands = new HashMap<>();
 
-    private void setUp() {
-        this.commands.put("1", new CommandImpl("Read a grammar from a file", () -> this.model = ContextFreeGrammarReader.readGrammar(this.view.readElement("File name: "), this.view.readElement("Symbols separator: "))));
-        this.commands.put("2", new CommandImpl("Display the Non-Terminals of the grammar", () -> this.view.printCollection("The NonTerminals are: ", this.model.nonTerminals(), ", ")));
-        this.commands.put("3", new CommandImpl("Display the Terminals of the grammar", () -> this.view.printCollection("The Terminals are: ", this.model.terminals(), ", ")));
-        this.commands.put("4", new CommandImpl("Display the start Non-Terminal if the grammar", () -> this.view.printText("The start Non-Terminal is: ", this.model.startNonTerminal())));
-        this.commands.put("5", new CommandImpl("Display the Productions of the grammar", () -> this.view.printCollection("The Productions are:\n", this.model.productions(), "\n")));
+    public Controller() {
     }
 
     public void run() {
@@ -55,5 +48,34 @@ public class Controller {
             }
         }
         this.view.printText("You chose to exit the application.\nHave a nice day! Bye.");
+    }
+
+    private void setUp() {
+        this.commands.put("1", new CommandImpl("Read a grammar from a file", () -> this.model = ContextFreeGrammarReader.readGrammar(this.view.readElement("File name: "), this.view.readElement("Symbols separator: "))));
+        this.commands.put("2", new CommandImpl("Display the Non-Terminals of the grammar", () -> {
+            this.validateGrammar();
+            this.view.printCollection("The NonTerminals are: ", this.model.nonTerminals(), ", ");
+        }));
+        this.commands.put("3", new CommandImpl("Display the Terminals of the grammar", () -> {
+            this.validateGrammar();
+            this.view.printCollection("The Terminals are: ", this.model.terminals(), ", ");
+        }));
+        this.commands.put("4", new CommandImpl("Display the start Non-Terminal if the grammar", () -> {
+            this.validateGrammar();
+            this.view.printText("The start Non-Terminal is: ", this.model.startNonTerminal());
+        }));
+        this.commands.put("5", new CommandImpl("Display the Productions of the grammar", () -> {
+            this.validateGrammar();
+            this.view.printCollection("The Productions are:\n", this.model.productions(), "\n");
+        }));
+        this.commands.put("6", new CommandImpl("Display the productions of a Non-Terminal", () -> {
+            this.validateGrammar();
+            final String nonTerminal = this.view.readElement("Non-Terminal: ");
+            this.view.printCollection(String.format("The productions of %s are:\n", nonTerminal), this.model.productionsOf(nonTerminal), "\n");
+        }));
+    }
+
+    private void validateGrammar() {
+        Objects.requireNonNull(this.model, "The grammar has not been read yet");
     }
 }
