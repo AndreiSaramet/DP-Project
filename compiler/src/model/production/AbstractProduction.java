9e09c61e797e2dd05bd3d723fd1272fd6763882b
AndreiSaramet <andreisaramet21@gmail.com>
2023-05-14 21:33:18 +0300

    model production refactor production & abstract production

--- compiler/src/model/production/AbstractProduction.java
+++ compiler/src/model/production/AbstractProduction.java
@@ -10,12 +10,20 @@ public abstract class AbstractProduction implements Production {
 
     protected final List<? extends Symbol> rightSide;
 
-    public AbstractProduction(final List<? extends Symbol> leftSide, final List<? extends Symbol> rightSide) {
-        Production.validateLeftSide(leftSide);
-        Production.validateRightSide(rightSide);
+    protected AbstractProduction(final List<? extends Symbol> leftSide, final List<? extends Symbol> rightSide) {
+        this.validateLeftSide(leftSide);
+        this.validateRightSide(rightSide);
 
         this.leftSide = leftSide;
         this.rightSide = rightSide;
+    }
+
+    protected void validateLeftSide(final List<? extends Symbol> leftSide) {
+        Objects.requireNonNull(leftSide, String.format("The left side of a %s must not be null", Production.class.getName()));
+    }
+
+    protected void validateRightSide(final List<? extends Symbol> rightSide) {
+        Objects.requireNonNull(rightSide, String.format("The right side of a %s must not be null", Production.class.getName()));
     }
 
     @Override
@@ -45,6 +53,19 @@ public abstract class AbstractProduction implements Production {
 
     @Override
     public String toString() {
-        return String.format("%s %s %s", Production.sideToString(this.leftSide), Production.SIDES_SEPARATOR, Production.sideToString(this.rightSide));
+        return String.format("%s %s %s", sideToString(this.leftSide), Production.SIDES_SEPARATOR, sideToString(this.rightSide));
+    }
+
+    private static String sideToString(final List<? extends Symbol> side) {
+        Objects.requireNonNull(side, String.format("A side of a %s must not be null", Production.class.getName()));
+        final StringBuilder builder = new StringBuilder();
+        if (side.isEmpty()) {
+            builder.append(EPSILON);
+        } else {
+            side.stream()
+                    .map(Symbol::value)
+                    .forEach(builder::append);
+        }
+        return builder.toString();
     }
 }
--- compiler/src/model/production/Production.java
+++ compiler/src/model/production/Production.java
@@ -3,7 +3,6 @@ package model.production;
 import model.vocabulary.Symbol;
 
 import java.util.List;
-import java.util.Objects;
 
 public interface Production {
     String EPSILON = "ε";
@@ -13,25 +12,4 @@ public interface Production {
     List<? extends Symbol> leftSide();
 
     List<? extends Symbol> rightSide();
-
-    static String sideToString(final List<? extends Symbol> side) {
-        Objects.requireNonNull(side, String.format("A side of a %s must not be null", Production.class.getName()));
-        final StringBuilder builder = new StringBuilder();
-        if (side.isEmpty()) {
-            builder.append(EPSILON);
-        } else {
-            side.stream()
-                    .map(Symbol::value)
-                    .forEach(builder::append);
-        }
-        return builder.toString();
-    }
-
-    static void validateLeftSide(final List<? extends Symbol> leftSide) {
-        Objects.requireNonNull(leftSide, String.format("The left side of a %s must not be null", Production.class.getName()));
-    }
-
-    static void validateRightSide(final List<? extends Symbol> rightSide) {
-        Objects.requireNonNull(rightSide, String.format("The right side of a %s must not be null", Production.class.getName()));
-    }
 }
