ff7373e745eba296c47aca388ead9f29f7498a3d
AndreiSaramet <andreisaramet21@gmail.com>
2023-05-14 16:02:37 +0300

    model production update context dependent production added builder

--- compiler/src/model/production/context_dependent/ContextDependentProduction.java
+++ compiler/src/model/production/context_dependent/ContextDependentProduction.java
@@ -1,6 +1,8 @@
 package model.production.context_dependent;
 
 import model.production.Production;
+import model.production.builder.ProductionBuilder;
+import model.production.builder.ProductionBuilderImpl;
 import model.production.unrestricted.UnrestrictedProduction;
 import model.vocabulary.Symbol;
 import model.vocabulary.nonterminal.NonTerminal;
@@ -24,5 +26,9 @@ public interface ContextDependentProduction extends UnrestrictedProduction {
 
     static void validateRightSide(final List<? extends Symbol> rightSide) {
         UnrestrictedProduction.validateRightSide(rightSide);
+    }
+
+    static ProductionBuilder<? extends ContextDependentProduction> builder() {
+        return new ProductionBuilderImpl<>(ContextDependentProductionImpl::new);
     }
 }
