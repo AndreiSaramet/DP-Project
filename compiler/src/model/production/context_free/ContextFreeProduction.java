474d46aaa95c65afa6ba2eb5a4f57c34b5126809
AndreiSaramet <andreisaramet21@gmail.com>
2023-05-14 21:36:01 +0300

    model production refactor context free production

--- compiler/src/model/production/context_free/ContextFreeProduction.java
+++ compiler/src/model/production/context_free/ContextFreeProduction.java
@@ -1,6 +1,5 @@
 package model.production.context_free;
 
-import model.production.Production;
 import model.production.builder.ProductionBuilder;
 import model.production.builder.ProductionBuilderImpl;
 import model.production.context_dependent.ContextDependentProduction;
@@ -9,25 +8,33 @@ import model.vocabulary.nonterminal.NonTerminal;
 
 import java.util.List;
 
-public interface ContextFreeProduction extends ContextDependentProduction {
-    NonTerminal leftSideSymbol();
-
-    static boolean isInstance(final Production production) {
-        return production instanceof ContextFreeProduction;
+public class ContextFreeProduction extends ContextDependentProduction {
+    protected ContextFreeProduction(final List<? extends Symbol> leftSide, final List<? extends Symbol> rightSide) {
+        super(leftSide, rightSide);
     }
 
-    static void validateLeftSide(final List<? extends Symbol> leftSide) {
-        ContextDependentProduction.validateLeftSide(leftSide);
+    protected ContextFreeProduction(final NonTerminal leftSide, final List<? extends Symbol> rightSide) {
+        super(List.of(leftSide), rightSide);
+    }
+
+    public NonTerminal leftSideSymbol() {
+        return (NonTerminal) this.leftSide.get(0);
+    }
+
+    @Override
+    protected void validateLeftSide(final List<? extends Symbol> leftSide) {
+        super.validateLeftSide(leftSide);
         if (leftSide.size() != 1) {
             throw new IllegalArgumentException(String.format("The left side of a %s must contain exactly one %s symbol", ContextFreeProduction.class.getName(), NonTerminal.class.getName()));
         }
     }
 
-    static void validateRightSide(final List<? extends Symbol> rightSide) {
-        ContextDependentProduction.validateRightSide(rightSide);
+    @Override
+    protected void validateRightSide(final List<? extends Symbol> rightSide) {
+        super.validateRightSide(rightSide);
     }
 
-    static ProductionBuilder<? extends ContextFreeProduction> builder() {
-        return new ProductionBuilderImpl<>(ContextFreeProductionImpl::new);
+    public static ProductionBuilder<? extends ContextFreeProduction> builder() {
+        return new ProductionBuilderImpl<>(ContextFreeProduction::new);
     }
 }
