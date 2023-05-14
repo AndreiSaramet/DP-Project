b36868029823604d9e1bdbdaccf8557520ca77f9
AndreiSaramet <andreisaramet21@gmail.com>
2023-05-15 01:49:30 +0300

    command interface + implementation

---
+++ compiler/src/command/CommandImpl.java
@@ -0,0 +1,22 @@
+package command;
+
+public class CommandImpl implements Command {
+    private final String message;
+    private final Runnable function;
+
+
+    public CommandImpl(final String message, final Runnable function) {
+        this.message = message;
+        this.function = function;
+    }
+
+    @Override
+    public void execute() {
+        this.function.run();
+    }
+
+    @Override
+    public String getName() {
+        return message;
+    }
+}
